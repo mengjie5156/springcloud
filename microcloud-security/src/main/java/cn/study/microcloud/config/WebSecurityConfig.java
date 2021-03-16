b817ab446f39f08d3c16db1fd034ce0adb25a449
jiamengjie <961798750@qq.com>
2021-03-16 16:29:31 +0800

    hystrixDashboard+Tubine

--- microcloud-security/src/main/java/cn/study/microcloud/config/WebSecurityConfig.java
+++ microcloud-security/src/main/java/cn/study/microcloud/config/WebSecurityConfig.java
@@ -1,16 +1,22 @@
 package cn.study.microcloud.config;
 
-import javax.annotation.Resource;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+
+import javax.annotation.Resource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/hystrix.stream","/turbine.stream") ;
+    }
+
     @Resource
     public void configGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
